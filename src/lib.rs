152640921622bb6ad47285ff115d33a8eb22d612
Grzegorz Dziadkiewicz <grzegorz@dziadkiewicz.com>
2025-02-04 14:39:07 +0100

    Refactor tests

--- src/lib.rs
+++ src/lib.rs
@@ -600,105 +600,210 @@ fn get_label_index(
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use assert2::check;
+    use assert2::assert;
 
     #[test]
-    fn simple_test() {
-        let simple_programs = &[
-            r#"
-; My first program
-mov  a, 5
-inc  a
-call function
-msg  '(5+1)/2 = ', a    ; output message
-end
+    fn test_simple_program_1() {
+        let program = r#"
+    ; My first program
+    mov  a, 5
+    inc  a
+    call function
+    msg  '(5+1)/2 = ', a    ; output message
+    end
 
-function:
-    div  a, 2
-    ret
-"#,
-            r#"
-mov   a, 5
-mov   b, a
-mov   c, a
-call  proc_fact
-call  print
-end
+    function:
+        div  a, 2
+        ret
+    "#;
+        let expected = Some(String::from("(5+1)/2 = 3"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
 
-proc_fact:
-    dec   b
-    mul   c, b
-    cmp   b, 1
-    jne   proc_fact
-    ret
+    #[test]
+    fn test_simple_program_2() {
+        let program = r#"
+    mov   a, 5
+    mov   b, a
+    mov   c, a
+    call  proc_fact
+    call  print
+    end
 
-print:
-    msg   a, '! = ', c ; output text
-    ret
-"#,
-            r#"
-mov   a, 8            ; value
-mov   b, 0            ; next
-mov   c, 0            ; counter
-mov   d, 0            ; first
-mov   e, 1            ; second
-call  proc_fib
-call  print
-end
+    proc_fact:
+        dec   b
+        mul   c, b
+        cmp   b, 1
+        jne   proc_fact
+        ret
 
-proc_fib:
-    cmp   c, 2
-    jl    func_0
-    mov   b, d
-    add   b, e
-    mov   d, e
-    mov   e, b
-    inc   c
-    cmp   c, a
-    jle   proc_fib
-    ret
+    print:
+        msg   a, '! = ', c ; output text
+        ret
+    "#;
+        let expected = Some(String::from("5! = 120"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
 
-func_0:
-    mov   b, c
-    inc   c
-    jmp   proc_fib
+    #[test]
+    fn test_simple_program_3() {
+        let program = r#"
+    mov   a, 8            ; value
+    mov   b, 0            ; next
+    mov   c, 0            ; counter
+    mov   d, 0            ; first
+    mov   e, 1            ; second
+    call  proc_fib
+    call  print
+    end
 
-print:
-    msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text
-    ret
-"#,
-            "\nmov   a, 11           ; value1\nmov   b, 3            ; value2\ncall  mod_func\nmsg   'mod(', a, ', ', b, ') = ', d        ; output\nend\n\n; Mod function\nmod_func:\n    mov   c, a        ; temp1\n    div   c, b\n    mul   c, b\n    mov   d, a        ; temp2\n    sub   d, c\n    ret\n",
-            "\nmov   a, 81         ; value1\nmov   b, 153        ; value2\ncall  init\ncall  proc_gcd\ncall  print\nend\n\nproc_gcd:\n    cmp   c, d\n    jne   loop\n    ret\n\nloop:\n    cmp   c, d\n    jg    a_bigger\n    jmp   b_bigger\n\na_bigger:\n    sub   c, d\n    jmp   proc_gcd\n\nb_bigger:\n    sub   d, c\n    jmp   proc_gcd\n\ninit:\n    cmp   a, 0\n    jl    a_abs\n    cmp   b, 0\n    jl    b_abs\n    mov   c, a            ; temp1\n    mov   d, b            ; temp2\n    ret\n\na_abs:\n    mul   a, -1\n    jmp   init\n\nb_abs:\n    mul   b, -1\n    jmp   init\n\nprint:\n    msg   'gcd(', a, ', ', b, ') = ', c\n    ret\n",
-            r#"
-call  func1
-call  print
-end
+    proc_fib:
+        cmp   c, 2
+        jl    func_0
+        mov   b, d
+        add   b, e
+        mov   d, e
+        mov   e, b
+        inc   c
+        cmp   c, a
+        jle   proc_fib
+        ret
 
-func1:
-    call  func2
-    ret
+    func_0:
+        mov   b, c
+        inc   c
+        jmp   proc_fib
 
-func2:
-    ret
+    print:
+        msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text
+        ret
+    "#;
+        let expected = Some(String::from("Term 8 of Fibonacci series is: 21"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
 
-print:
-    msg 'This program should return null'
-"#,
-            "\nmov   a, 2            ; value1\nmov   b, 10           ; value2\nmov   c, a            ; temp1\nmov   d, b            ; temp2\ncall  proc_func\ncall  print\nend\n\nproc_func:\n    cmp   d, 1\n    je    continue\n    mul   c, a\n    dec   d\n    call  proc_func\n\ncontinue:\n    ret\n\nprint:\n    msg a, '^', b, ' = ', c\n    ret\n"
-            ];
+    #[test]
+    fn test_simple_program_4() {
+        let program = r#"
+    mov   a, 11           ; value1
+    mov   b, 3            ; value2
+    call  mod_func
+    msg   'mod(', a, ', ', b, ') = ', d        ; output
+    end
 
-        let expected = &[
-            Some(String::from("(5+1)/2 = 3")),
-            Some(String::from("5! = 120")),
-            Some(String::from("Term 8 of Fibonacci series is: 21")),
-            Some(String::from("mod(11, 3) = 2")),
-            Some(String::from("gcd(81, 153) = 9")),
-            None,
-            Some(String::from("2^10 = 1024")),
-        ];
+    ; Mod function
+    mod_func:
+        mov   c, a        ; temp1
+        div   c, b
+        mul   c, b
+        mov   d, a        ; temp2
+        sub   d, c
+        ret
+    "#;
+        let expected = Some(String::from("mod(11, 3) = 2"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
 
-        for (prg, exp) in simple_programs.iter().zip(expected) {
-            check!(AssemblerInterpreter::interpret_to_result(*prg).as_ref() == Ok(exp));
-        }
+    #[test]
+    fn test_simple_program_5() {
+        let program = r#"
+    mov   a, 81         ; value1
+    mov   b, 153        ; value2
+    call  init
+    call  proc_gcd
+    call  print
+    end
+
+    proc_gcd:
+        cmp   c, d
+        jne   loop
+        ret
+
+    loop:
+        cmp   c, d
+        jg    a_bigger
+        jmp   b_bigger
+
+    a_bigger:
+        sub   c, d
+        jmp   proc_gcd
+
+    b_bigger:
+        sub   d, c
+        jmp   proc_gcd
+
+    init:
+        cmp   a, 0
+        jl    a_abs
+        cmp   b, 0
+        jl    b_abs
+        mov   c, a            ; temp1
+        mov   d, b            ; temp2
+        ret
+
+    a_abs:
+        mul   a, -1
+        jmp   init
+
+    b_abs:
+        mul   b, -1
+        jmp   init
+
+    print:
+        msg   'gcd(', a, ', ', b, ') = ', c
+        ret
+    "#;
+        let expected = Some(String::from("gcd(81, 153) = 9"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
+
+    #[test]
+    fn test_simple_program_6() {
+        let program = r#"
+    call  func1
+    call  print
+    end
+
+    func1:
+        call  func2
+        ret
+
+    func2:
+        ret
+
+    print:
+        msg 'This program should return null'
+    "#;
+        let expected = None;
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
+    }
+
+    #[test]
+    fn test_simple_program_7() {
+        let program = r#"
+    mov   a, 2            ; value1
+    mov   b, 10           ; value2
+    mov   c, a            ; temp1
+    mov   d, b            ; temp2
+    call  proc_func
+    call  print
+    end
+
+    proc_func:
+        cmp   d, 1
+        je    continue
+        mul   c, a
+        dec   d
+        call  proc_func
+
+    continue:
+        ret
+
+    print:
+        msg a, '^', b, ' = ', c
+        ret
+    "#;
+        let expected = Some(String::from("2^10 = 1024"));
+        assert!(AssemblerInterpreter::interpret_to_result(program).as_ref() == Ok(&expected));
     }
 }
